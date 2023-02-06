aec1402005bb33ba17fa43c5d4e2fabc06ad21b2
bang07112004 <phong0909191145@gmail.com>
2023-02-06 14:35:42 +0700

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,5 +4,5 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return <h1 className="text-xl font-bold text-red-400">Home</h1>;
 }
